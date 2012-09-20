c6b8a53c41e2eef8b9a4e86b6f0d5e1448bc47f0
Natalia <Natalia.Terlecka@gmail.com>
2012-09-20 20:06:02 +0200

    Walleet user class added.

---
+++ wallet/WalleetUserData.h
@@ -0,0 +1,18 @@
+//
+//  WalleetUserData.h
+//  wallet
+//
+//  Created by Natalia Terlecka on 9/20/12.
+//  Copyright (c) 2012 Natalia Terlecka. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WalleetUserData : NSObject
+
+@property (nonatomic, strong) NSString *userToken;
+@property (nonatomic, strong) NSString *userEmail;
+@property (nonatomic, strong) NSString *userPassword;
+
+
+@end
