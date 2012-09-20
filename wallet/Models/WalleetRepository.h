261a1166b977a76d8c2a6effe715ec36972cb60d
Android <android@futuresimple.com>
2012-09-20 19:15:02 +0200

    - WalleetRepository files added.

---
+++ wallet/Models/WalleetRepository.h
@@ -0,0 +1,13 @@
+//
+//  WalleetRepository.h
+//  wallet
+//
+//  Created by Future Simple on 9/20/12.
+//  Copyright (c) 2012 Natalia Terlecka. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WalleetRepository : NSObject
+
+@end
---
+++ wallet/Models/WalleetRepository.m
@@ -0,0 +1,13 @@
+//
+//  WalleetRepository.m
+//  wallet
+//
+//  Created by Future Simple on 9/20/12.
+//  Copyright (c) 2012 Natalia Terlecka. All rights reserved.
+//
+
+#import "WalleetRepository.h"
+
+@implementation WalleetRepository
+
+@end
